0da1102590f338e820603e367c2f6fb33dcc6509
Michael Davis <jrmdd1496@gmail.com>
2022-02-28 13:46:36 -0600

    commit tree 7c1476d2adddb284056d9deac5723a6460197845

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+
+function scuberGreetingForFeet(someValue){
+
+      if (someValue > 2500){
+        return 'No can do.';
+      }
+      else if (someValue <= 2500 && someValue > 2000 ){
+        return 'I will gladly take your thirty bucks.';
+      }
+      else if (someValue <= 400 ){
+        return 'This one is on me!';
+      }
+    }
+
+
+
+function ternaryCheckCity(cityName){
+  let travelChoice = (cityName === 'NYC')? 'Ok, sounds good.' : 'No go.';
+  return travelChoice;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function switchOnCharmFromTip(generousity){
+  switch (generousity){
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
+  
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+console.log(switchOnCharmFromTip('generous'));
